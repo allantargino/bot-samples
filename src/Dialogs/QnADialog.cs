5ef403fcf8bd419dc6aa3e20a0b1b501536509e8
Allan Targino <allan_targino@hotmail.com>
2018-09-11 10:56:05 -0300

    qnamaker dialog

---
+++ src/Dialogs/QnADialog.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using QnAMakerDialog;
+using QnAMakerDialog.Models;
+
+namespace bot_sample.Dialogs
+{
+    [Serializable]
+    public class QnADialog : QnAMakerDialog<object>
+    {
+        public QnADialog(string baseUri, string knowledgeBaseId, string endpointKey)
+        {
+            this.BaseUri = baseUri;
+            this.KnowledgeBaseId = knowledgeBaseId;
+            this.EndpointKey = endpointKey;
+        }
+
+        public override Task StartAsync(IDialogContext context)
+        {
+            return base.StartAsync(context);
+        }
+
+        public override Task DefaultMatchHandler(IDialogContext context, string originalQueryText, QnAMakerResult result)
+        {
+            return base.DefaultMatchHandler(context, originalQueryText, result);
+        }
+
+        public override Task NoMatchHandler(IDialogContext context, string originalQueryText)
+        {
+            return base.NoMatchHandler(context, originalQueryText);
+        }
+    }
+}
